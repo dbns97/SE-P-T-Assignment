c4dbd306dd1fcc1728c01f3f4e5eba032a53e007
shawnotaylor <shawnotaylor04@gmail.com>
2017-03-16 17:25:51 +1100

    starting login function
    also added the databases (JSON files)

--- code/src/PublicMenu.java
+++ code/src/PublicMenu.java
@@ -10,8 +10,35 @@ public class PublicMenu extends Menu{
 		sc = new Scanner(System.in);
 	}
 	
-	public void run(){
-		
+	public void run()
+    {
+		int choice;
+   
+        System.out.println("   Appointment Booking System   ");
+        System.out.println("--------------------------------");
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("3. Exit");
+        System.out.println("Please choose an option (1,2,3)");
+
+        choice = sc.nextInt();
+
+        switch(choice)
+        {
+         case 1:
+            Boolean didLogin = login();
+            System.out.println(didLogin);
+            break;
+         case 2:
+            Boolean didRegister = register();
+            System.out.println(didRegister);
+            break;
+         case 3:
+            exit();
+            break;
+         default:
+            System.out.println("error");
+        }	   
 	}
 	
 	public boolean register()
