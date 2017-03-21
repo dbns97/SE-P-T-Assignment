4e5bc4cae7dba0bd64671655375ae94ffc57ed69
Drew Nuttall-Smith <drew@mysticpants.com>
2017-03-21 16:57:09 +1100

    Fixed indentation

--- code/src/JSONUtils.java
+++ code/src/JSONUtils.java
@@ -6,36 +6,36 @@ import org.json.JSONObject;
 
 public class JSONUtils
 {
-   public static String getJSONStringFromFile(String path)
-   {
-      Scanner sc;
-      InputStream in = FileHandle.inputStreamFromFile(path);
-      sc = new Scanner(in);
-      String json = sc.useDelimiter("\\z").next();
-      sc.close();
-      
-      return json;
-      
-   }
-   
-   public static JSONObject getJSONObjectFromFile(String path)
-   {
-      return new JSONObject(getJSONStringFromFile(path));
-   }
-   
-   public static boolean objectExists(JSONObject jsonObject, String key)
-   {
-      Object o;
-      
-      try
-      {
-         o = jsonObject.get(key);
-      }
-      catch(Exception e)
-      {
-         return false;
-      }
-      
-      return o != null;
-   }
+    public static String getJSONStringFromFile(String path)
+    {
+        Scanner sc;
+        InputStream in = FileHandle.inputStreamFromFile(path);
+        sc = new Scanner(in);
+        String json = sc.useDelimiter("\\z").next();
+        sc.close();
+
+        return json;
+
+    }
+
+    public static JSONObject getJSONObjectFromFile(String path)
+    {
+        return new JSONObject(getJSONStringFromFile(path));
+    }
+
+    public static boolean objectExists(JSONObject jsonObject, String key)
+    {
+        Object o;
+
+        try
+        {
+            o = jsonObject.get(key);
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+
+        return o != null;
+    }
 }
