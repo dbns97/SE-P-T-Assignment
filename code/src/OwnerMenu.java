9f2bfd0f0fca3f2522481cf16e94049cb9e06feb
Drew Nuttall-Smith <drew@mysticpants.com>
2017-03-27 01:37:58 +1100

    Wrote basic addEmployee() function. Needs testing.

--- code/src/OwnerMenu.java
+++ code/src/OwnerMenu.java
@@ -80,6 +80,59 @@ public class OwnerMenu extends Menu{
 
 	public boolean addEmployee()
 	{
+		String name;
+		String email;
+		JSONObject newEmployee;
+		JSONArray employees = busInfo.getJSONArray("employees");
+
+		do
+		{
+			// Get employee name
+			System.out.println("Please enter name of employee:");
+			name = sc.nextLine();
+
+			// If user doesn't enter anything, cancel this operation
+			if (name.length() == 0) return false;
+
+			// If invalid format, print error message
+	        if(!name.matches("[a-zA-Z ,.'-]+")) {
+				System.out.println("Invalid name");
+				continue;
+			} else {
+				break;
+			}
+
+		} while (!name.matches("[a-zA-Z ,.'-]+"));
+
+		do
+		{
+			// Get employee email
+			System.out.println("Please enter email of employee:");
+			email = sc.nextLine();
+
+			// If user doesn't enter anything, cancel this operation
+			if (email.length() == 0) return false;
+
+			// If invalid format, print error message
+	        if(!email.matches("[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]")) {
+				System.out.println("Invalid email");
+				continue;
+			} else {
+				break;
+			}
+		} while (!email.matches("[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]"));
+
+		// Populate JSON object with employee info
+        newEmployee = new JSONObject();
+        newEmployee.put("name", name);
+        newEmployee.put("email", email);
+        newEmployee.put("workingTime", new JSONArray());
+        newEmployee.put("workingDates", new JSONArray());
+
+		// Write employee JSON object to business data JSON object
+		employess.put(newEmployee);
+		busInfo.put("employees", employees);
+
 		return false;
 	}
 
@@ -105,12 +158,47 @@ public class OwnerMenu extends Menu{
 
 	public void logout()
 	{
-
+		writeToFiles();
 	}
 
 	public void exit()
 	{
+		writeToFiles();
 		System.exit(0);
+	}
+
+	public void writeToFiles() {
+		// Write to business data file
+        try
+        {
+            FileWriter busFile = new FileWriter("business.json");
+            busFile.write(busInfo.toJSONString());
+        }
+        catch (FileNotFoundException e)
+        {
+			System.out.println("Error writing to business data file");
+        }
+		finally
+		{
+            busFile.flush();
+            busFile.close();
+		}
+
+		// Write to customer data file
+        try
+        {
+            FileWriter custFile = new FileWriter("customerinfo.json");
+            custFile.write(custInfo.toJSONString());
+        }
+        catch (FileNotFoundException e)
+        {
+			System.out.println("Error writing to customer data file");
+        }
+		finally
+		{
+            custFile.flush();
+            custFile.close();
+		}
 	}
 
 }
