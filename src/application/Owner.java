c5932fd968e67981670bab3f9a71294d3f261cc8
astefanovic <astefanovic101@gmail.com>
2017-03-27 01:52:15 +1100

    Leftover from last commit

--- src/application/Owner.java
+++ src/application/Owner.java
@@ -2,6 +2,11 @@ package application;
 
 public class Owner extends User {
 
+	private String username; 
+	private String password; 
+	private String address;
+	private int contactNumber;
+	
 	@Override
 	public String toJSONString() {
 		String output = "";
