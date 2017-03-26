529d1bf9d4c824273600132845c8f879fdd15824
astefanovic <astefanovic101@gmail.com>
2017-03-27 00:08:40 +1100

    Added UI for MainMenu

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,17 +3,21 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	public Stage primaryStage;
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Booking System");
+		
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+			Scene scene = new Scene(root,346.0,290.0);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
