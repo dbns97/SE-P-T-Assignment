143e392c1fdaad611924320bd6174517afcd07b0
Drew Nuttall-Smith <drew@mysticpants.com>
2017-03-28 01:44:54 +1100

    Wrote addShift(). Minor adjustments to addEmployee().

--- code/src/OwnerMenu.java
+++ code/src/OwnerMenu.java
@@ -86,7 +86,7 @@ public class OwnerMenu extends Menu{
 	* @param none
 	* @return boolean - whether the new employee was added
 	* @author Drew Nuttall-Smith
-	* @since 26/03/2017
+	* @since 27/03/2017
 	*/
 	public boolean addEmployee()
 	{
@@ -95,6 +95,7 @@ public class OwnerMenu extends Menu{
 		boolean uniqueEmail;
 		JSONObject newEmployee;
 		JSONObject employees = busInfo.getJSONObject("employees");
+		String nameRegex = "[a-zA-Z ,.'-]+";
 		String emailRegex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]+";
 
 		do
@@ -107,14 +108,14 @@ public class OwnerMenu extends Menu{
 			if (name.length() == 0) return false;
 
 			// If invalid format, print error message
-	        if(!name.matches("[a-zA-Z ,.'-]+")) {
+	        if(!name.matches(nameRegex)) {
 				System.out.println("Invalid name");
 				continue;
 			} else {
 				break;
 			}
 
-		} while (!name.matches("[a-zA-Z ,.'-]+"));
+		} while (!name.matches(nameRegex));
 
 		do
 		{
@@ -147,8 +148,7 @@ public class OwnerMenu extends Menu{
 		// Populate JSON object with employee info
         newEmployee = new JSONObject();
         newEmployee.put("name", name);
-        newEmployee.put("workingTime", new JSONArray());
-        newEmployee.put("workingDates", new JSONArray());
+        newEmployee.put("shifts", new JSONObject());
 
 		// Write employee JSON object to business data JSON object
 		employees.put(email, newEmployee);
@@ -158,12 +158,97 @@ public class OwnerMenu extends Menu{
 		System.out.println("Name: " + name);
 		System.out.println("Name: " + email + "\n");
 
-		return false;
+		// TESTING: this log is for testing purposes
+		System.out.println(newEmployee.toString());
+
+		return true;
 	}
 
+	/**
+	* Add a shift for an employee
+	*
+	* @param none
+	* @return boolean - whether the new shift was successfully added
+	* @author Drew Nuttall-Smith
+	* @since 27/03/2017
+	*/
 	public boolean addShift()
 	{
-		return false;
+		JSONObject employees = busInfo.getJSONObject("employees");
+		boolean employeeExists = false;
+		String email;
+		String date;
+		int startTime;
+		int endTime;
+		String dateRegex = "[0-9]{2}/[0-9]{2}/[0-9]{4}";
+
+		do
+		{
+			// Get employee to add shift for
+			System.out.println("Enter employee email:");
+			email = sc.nextLine();
+
+			// If user doesn't enter anything, cancel this operation
+			if (email.length() == 0) return false;
+
+			// Check if email matches an existing employee
+	        String[] emails = JSONObject.getNames(employees);
+			for (String e : emails) {
+				if (e.equals(email)) {
+					employeeExists = true;
+					break;
+				}
+			}
+			// If no employee exists with entered email address, prompt user to re-enter email
+			if (!employeeExists) {
+				System.out.println("No employee exists with that email");
+				continue;
+			}
+		} while (!employeeExists);
+
+		do
+		{
+			// Get date for shift
+			System.out.println("Enter date of shift:");
+			date = sc.nextLine();
+
+			// If user doesn't enter anything, cancel this operation
+			if (date.length() == 0) return false;
+
+			// If invalid format, print error message
+	        if(!date.matches(dateRegex)) {
+				System.out.println("Invalid date");
+				continue;
+			} else {
+				break;
+			}
+		} while (!date.matches(dateRegex));
+
+		// Get start time for shift
+		System.out.println("Enter start time of shift (as integer in 24hr time. e.g. 9):");
+		startTime = sc.nextInt();
+		// Get start time for shift
+		System.out.println("Enter end time of shift (as integer in 24hr time. e.g. 17):");
+		endTime = sc.nextInt();
+
+		// Remove slashes from date
+		date = date.replace("/", "");
+
+		// Add shift to JSON
+		JSONObject employee = employees.getJSONObject(email);
+		JSONObject shifts = employee.getJSONObject("shifts");
+		JSONArray shift = new JSONArray();
+		shift.put(startTime);
+		shift.put(endTime);
+		shifts.put(date, shift);
+		employee.put("shifts", shifts);
+		employees.put(email, employee);
+		busInfo.put("employees", employees);
+		
+		// TESTING: this log is for testing purposes
+		System.out.println(shifts.toString());
+
+		return true;
 	}
 
 	public void viewBookings()
