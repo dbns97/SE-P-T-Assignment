dd930966024052678ea97a2fe3f186c7d8516bc5
astefanovic <astefanovic101@gmail.com>
2017-05-28 22:30:40 +1000

    Finished OwnerMakeBookingTests

--- src/application/tests/CustomerMakeBookingTests.java
+++ src/application/tests/CustomerMakeBookingTests.java
@@ -24,7 +24,7 @@ import application.models.Owner;
 import application.models.Service;
 
 @RunWith(Parameterized.class)
-public class CustomerAddBookingTests {
+public class CustomerMakeBookingTests {
 
 	@Parameters
 	public static Collection<Object[]> data() 
